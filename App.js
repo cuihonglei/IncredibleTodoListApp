d98f8b0210d7805b1c1b1e9474c355a16ff0f214
Robin Cui <honglei9423@gmail.com>
2024-03-07 15:59:32 -0700

    Use the map function to dynamically render each task as a list item on the screen.

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import { SafeAreaView } from 'react-native';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 
+
 export default function App() {
+
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+
   return (
     <SafeAreaView>
-      <TodoList />
+      <TodoList tasks={tasks} />
       <TodoForm />
     </SafeAreaView>
   );
